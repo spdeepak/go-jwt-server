6472215b13ea1d0263fd7818d508e1c8fb26dca6
Deepak Sunanda Prabhakar <deepak.prabhakar@trendyol.com>
2025-05-20 19:55:59 +0200

    fix

--- users/service.go
+++ users/service.go
@@ -68,7 +68,7 @@ func (s *service) Login(ctx *gin.Context, params api.LoginParams, login api.User
 }
 
 func (s *service) RefreshToken(ctx *gin.Context, params api.RefreshParams, refresh api.Refresh) (api.LoginResponse, error) {
-	_, claims, err := s.tokenService.VerifyRefreshToken(ctx, refresh.RefreshToken)
+	claims, err := s.tokenService.VerifyRefreshToken(ctx, refresh.RefreshToken)
 	if err != nil {
 		return api.LoginResponse{}, err
 	}
