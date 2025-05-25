a8f02c709c597f90d801a29f44fbd4191fb4a349
Deepak Sunanda Prabhakar <deepak.prabhakar@trendyol.com>
2025-05-25 15:12:21 +0200

    add indexes

--- migrations/0001_initial.up.sql
+++ migrations/0001_initial.up.sql
@@ -11,12 +11,22 @@ CREATE TABLE IF NOT EXISTS jwt_secrets
 
 CREATE TABLE IF NOT EXISTS users
 (
-    email      TEXT PRIMARY KEY,
+    id         UUID PRIMARY KEY     DEFAULT uuid_generate_v4(),
+    email      TEXT UNIQUE,
     first_name TEXT        NOT NULL,
     last_name  TEXT        NOT NULL,
     password   TEXT        NOT NULL,
+    locked     BOOLEAN     NOT NULL DEFAULT FALSE,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_password
+(
+    user_id UUID NOT NULL,
+    password   TEXT        NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE INDEX IF NOT EXISTS users_email ON users (email);
@@ -28,7 +38,7 @@ CREATE TABLE IF NOT EXISTS tokens
     issued_at          TIMESTAMPTZ NOT NULL DEFAULT now(),
     token_expires_at   TIMESTAMPTZ NOT NULL,
     refresh_expires_at TIMESTAMPTZ NOT NULL,
-    revoked            BOOLEAN              DEFAULT FALSE,
+    revoked            BOOLEAN     NOT NULL DEFAULT FALSE,
     revoked_at         TIMESTAMPTZ,
     email              TEXT        NOT NULL,
     ip_address         TEXT        NOT NULL,
@@ -37,6 +47,8 @@ CREATE TABLE IF NOT EXISTS tokens
     created_by         TEXT        NOT NULL --Source of request web/mobile/api
 );
 
-CREATE INDEX IF NOT EXISTS tokens_token ON tokens (token);
-CREATE INDEX IF NOT EXISTS tokens_refresh_token ON tokens (refresh_token);
-CREATE INDEX IF NOT EXISTS tokens_email ON tokens (email);
+CREATE INDEX IF NOT EXISTS idx_tokens_token ON tokens (token);
+CREATE INDEX IF NOT EXISTS idx_tokens_refresh_token ON tokens (refresh_token);
+CREATE INDEX IF NOT EXISTS idx_tokens_email ON tokens (email);
+CREATE INDEX IF NOT EXISTS idx_bearer_valid ON tokens (token, ip_address, user_agent, device_name, revoked);
+CREATE INDEX IF NOT EXISTS idx_refresh_valid ON tokens (refresh_token, ip_address, user_agent, device_name, revoked);
